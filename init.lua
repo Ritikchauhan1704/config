c424fc2d0c16a104c1599bcd29f54da9e7ea1a3d
RChauhan03 <chauhan.ritik.1704@gmail.com>
2025-05-14 14:07:20 +0530

    a

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+vim.opt.termguicolors = true -- Enable terminal true colors
+vim.g.encoding = "UTF-8"     -- Set encoding to UTF-8
+
+
 require("config.options")
 require("config.keybinds")
 require("config.lazy")
